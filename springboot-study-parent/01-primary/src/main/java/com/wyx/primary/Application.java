1bf3d02c7f0ac8e0ec29965f3406b7d5ea0842a3
yuxiang_wen <wenyvxiang@iiasaas.com>
2020-09-09 11:35:56 +0800

    add

---
+++ springboot-study-parent/01-primary/src/main/java/com/wyx/primary/Application.java
@@ -0,0 +1,16 @@
+package com.wyx.primary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 基础springboot项目
+ */
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
